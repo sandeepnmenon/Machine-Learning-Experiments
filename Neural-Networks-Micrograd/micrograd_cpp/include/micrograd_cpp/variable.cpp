7fa7690b1cdce85c908188013e09dfff99820e7b
Sandeep Menon <menonsandu@gmail.com>
2023-01-09 20:20:53 -0500

    variable class and backprop added

--- Neural-Networks-Micrograd/micrograd_cpp/include/micrograd_cpp/variable.cpp
+++ Neural-Networks-Micrograd/micrograd_cpp/include/micrograd_cpp/variable.cpp
@@ -15,32 +15,14 @@ namespace micrograd_cpp
 
     class Variable : public enable_shared_from_this<Variable>
     {
-    public:
-        float data;
-        float grad;
+        float _data;
         vector<VariablePtr> _prev;
         string op;
-        string label;
-        function<void()> backward;
+        string _label;
+        bool topo_visited = false;
 
         const vector<VariablePtr> &children() const { return _prev; }
-
-        Variable(float data, vector<VariablePtr> _children = vector<VariablePtr>(), string op = "", string label = "")
-        {
-            this->data = data;
-            this->grad = 0.0;
-            this->_prev = _children;
-            this->op = op;
-            this->label = label;
-            this->backward = []() {};
-        }
-
-        // function to print label when printing the object using cout
-        friend ostream &operator<<(ostream &os, const Variable &v)
-        {
-            os << "Variable(data=" << v.data << ", grad=" << v.grad << ", label=" << v.label << ")";
-            return os;
-        }
+        void set_visited() { topo_visited = true; }
 
         // Overload the + operator
         VariablePtr operator+(const VariablePtr other)
@@ -48,8 +30,8 @@ namespace micrograd_cpp
             VariablePtr self = shared_from_this();
             vector<VariablePtr> children = vector<VariablePtr>{self, other};
 
-            VariablePtr out = make_shared<Variable>(self->data + other->data, children, "+");
-            out->backward = [out, other, self]()
+            VariablePtr out = make_shared<Variable>(self->data() + other->data(), children, "+");
+            out->_backward = [out, other, self]()
             {
                 self->grad += 1.0 * out->grad;
                 other->grad += 1.0 * out->grad;
@@ -61,38 +43,98 @@ namespace micrograd_cpp
         // Overload the * operator
         VariablePtr operator*(const VariablePtr &other)
         {
-            VariablePtr out = make_shared<Variable>(this->data * other->data, vector<VariablePtr>{shared_from_this(), other}, "*");
-            out->backward = [out, other, self = shared_from_this()]()
+            VariablePtr out = make_shared<Variable>(this->data() * other->data(), vector<VariablePtr>{shared_from_this(), other}, "*");
+            out->_backward = [out, other, self = shared_from_this()]()
             {
-                self->grad += other->data * out->grad;
-                other->grad += self->data * out->grad;
+                self->grad += other->data() * out->grad;
+                other->grad += self->data() * out->grad;
             };
 
             return out;
+        }
+
+        void build_topo(VariablePtr var, vector<VariablePtr>& topo_order)
+        {
+            if(!var->topo_visited)
+            {
+                var->set_visited();
+                for(VariablePtr child: var->children())
+                {
+                    build_topo(child, topo_order);
+                }
+                topo_order.push_back(var);
+            }
+        }
+
+        void clear_topo_visited(vector<VariablePtr>& topo_order)
+        {
+            for(VariablePtr node: topo_order)
+            {
+                node->topo_visited = false;
+            }
+        }
+
+    public:
+        float grad;
+        function<void()> _backward;
+
+        void label(string label) { this->_label = label; }
+        float data() const { return _data; }
+
+        
+        Variable(float data, vector<VariablePtr> _children = vector<VariablePtr>(), string op = "", string label = "")
+        {
+            this->_data = data;
+            this->grad = 0.0;
+            this->_prev = _children;
+            this->op = op;
+            this->_label = label;
+            this->_backward = []() {};
+        }
+        
+        // function to print label when printing the object using cout
+        friend ostream &operator<<(ostream &os, const Variable &v)
+        {
+            os << "Variable(data=" << v.data() << ", grad=" << v.grad << ", label=" << v._label << ")";
+            return os;
         }
 
         // Define the pow operator
         VariablePtr pow(float exponent)
         {
             VariablePtr self = shared_from_this();
-            VariablePtr out = make_shared<Variable>(std::pow(this->data, exponent), vector<VariablePtr>{self}, "^" + to_string(exponent));
-            out->backward = [out, exponent, self]()
+            VariablePtr out = make_shared<Variable>(std::pow(this->data(), exponent), vector<VariablePtr>{self}, "^" + to_string(exponent));
+            out->_backward = [out, exponent, self]()
             {
-                self->grad += exponent * std::pow(self->data, exponent - 1) * out->grad;
+                self->grad += exponent * std::pow(self->data(), exponent - 1) * out->grad;
             };
 
             return out;
         }
 
+        // Define backpropagation function
+        void backward()
+        {
+            this->grad = 1.0;
+            vector<VariablePtr> topo_order;
+            build_topo(shared_from_this(), topo_order);
+            reverse(topo_order.begin(), topo_order.end());
+            for(VariablePtr node: topo_order)
+            {
+                node->_backward();
+            }
+            clear_topo_visited(topo_order);
+        }
+
         // Define the tanh function
         VariablePtr tanh()
         {
-            float n = this->data;
+            float n = this->data();
             float tanh_value = (exp(2 * n) - 1) / (exp(2 * n) + 1);
             VariablePtr out = make_shared<Variable>(tanh_value, vector<VariablePtr>{shared_from_this()}, "tanh");
-            out->backward = [out, self = shared_from_this()]()
+            out->_backward = [out, self = shared_from_this()]()
             {
-                self->grad += (1 - out->data * out->data) * out->grad;
+                self->grad += (1 - out->data() * out->data()) * out->grad;
             };
 
             return out;
@@ -101,12 +143,12 @@ namespace micrograd_cpp
         // Define the relu function
         VariablePtr relu()
         {
-            float n = this->data;
+            float n = this->data();
             float relu_value = n > 0 ? n : 0;
             VariablePtr out = make_shared<Variable>(relu_value, vector<VariablePtr>{shared_from_this()}, "relu");
-            out->backward = [out, self = shared_from_this()]()
+            out->_backward = [out, self = shared_from_this()]()
             {
-                self->grad += (out->data > 0 ? 1 : 0) * out->grad;
+                self->grad += (out->data() > 0 ? 1 : 0) * out->grad;
             };
 
             return out;
@@ -115,11 +157,11 @@ namespace micrograd_cpp
 
     inline VariablePtr operator*(VariablePtr lhs, VariablePtr rhs)
     {
-        VariablePtr out = make_shared<Variable>(lhs->data * rhs->data, vector<VariablePtr>{lhs, rhs}, "*");
-        out->backward = [out, lhs, rhs]()
+        VariablePtr out = make_shared<Variable>(lhs->data() * rhs->data(), vector<VariablePtr>{lhs, rhs}, "*");
+        out->_backward = [out, lhs, rhs]()
         {
-            lhs->grad += rhs->data * out->grad;
-            rhs->grad += lhs->data * out->grad;
+            lhs->grad += rhs->data() * out->grad;
+            rhs->grad += lhs->data() * out->grad;
         };
 
         return out;
@@ -137,8 +179,8 @@ namespace micrograd_cpp
 
     inline VariablePtr operator+(VariablePtr lhs, VariablePtr rhs)
     {
-        VariablePtr out = make_shared<Variable>(lhs->data + rhs->data, vector<VariablePtr>{lhs, rhs}, "+");
-        out->backward = [out, lhs, rhs]()
+        VariablePtr out = make_shared<Variable>(lhs->data() + rhs->data(), vector<VariablePtr>{lhs, rhs}, "+");
+        out->_backward = [out, lhs, rhs]()
         {
             lhs->grad += out->grad;
             rhs->grad += out->grad;
--- Neural-Networks-Micrograd/micrograd_cpp/tests/variable_test.cpp
+++ Neural-Networks-Micrograd/micrograd_cpp/tests/variable_test.cpp
@@ -3,15 +3,47 @@
 #include <string>
 #include <vector>
 #include <micrograd_cpp/variable.cpp>
-using namespace micrograd_cpp;
+using namespace std;
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
-    auto a = make_shared<Variable>(-4.0f);
-    auto b = make_shared<Variable>(2.0f);
-    auto c = a + b;
+    // Inputs x1 and x2
+    auto x1 = make_shared<micrograd_cpp::Variable>(2.0f);
+    x1->label("x1");
+    auto x2 = make_shared<micrograd_cpp::Variable>(1.0f);
+    x2->label("x2");
 
-    std::cout<<*c<<std::endl;
+    // Two weights for each input
+    auto w1 = make_shared<micrograd_cpp::Variable>(-3.0f);
+    w1->label("w1");
+    auto w2 = make_shared<micrograd_cpp::Variable>(0.0f);
+    w2->label("w2");
 
+    // Bias
+    auto b = make_shared<micrograd_cpp::Variable>(8.0f);
+    b->label("b");
+
+    // Applying sum of inputs with weights
+    auto x1w1 = x1 * w1;
+    x1w1->label("x1w1");
+    auto x2w2 = x2 * w2;
+    x2w2->label("x2w2");
+    auto x1w1x2w2 = x1w1 + x2w2;
+    x1w1x2w2->label("x1w1x2w2");
+    auto neuron1=  x1w1x2w2 + b;
+    neuron1->label("neuron1");
+    auto output = neuron1->tanh();
+    output->label("output");
+
+    cout<<*output<<endl;
+    // Backpropagation
+    output->backward();
+
+    cout<<*output<<endl;
+    cout<<*neuron1<<endl;
+
+    cout<<*w1<<endl;
+    cout<<*w2<<endl;
+  
     return 0;
 }
